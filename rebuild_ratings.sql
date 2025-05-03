fc6be0c9f7156c4a6194d92f724bde88d2f3a39e
los9111 <149305944+los9111@users.noreply.github.com>
2025-05-03 18:30:15 +0000

    Update all files, including new files and modifications

---
+++ rebuild_ratings.sql
@@ -0,0 +1,22 @@
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+ALTER TABLE ratings RENAME TO ratings_old;
+
+CREATE TABLE ratings (
+  id         INTEGER PRIMARY KEY,
+  rating     INTEGER    NULL,
+  project_id INTEGER    NOT NULL REFERENCES projects(id),
+  comment    TEXT       NULL,
+  created_at DATETIME   DEFAULT CURRENT_TIMESTAMP,
+  approved   BOOLEAN    DEFAULT 0 NOT NULL
+);
+
+INSERT INTO ratings (id, rating, project_id, comment, created_at, approved)
+  SELECT id, rating, project_id, comment, created_at, approved
+    FROM ratings_old;
+
+DROP TABLE ratings_old;
+COMMIT;
+
+PRAGMA foreign_keys = on;
